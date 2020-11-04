02f805e34644a01a13ed1c78e94c2e62189b6b55
David5627 <binginaya708@gmail.com>
2020-11-04 13:05:35 +0300

    first commit

---
+++ src/app/strikethrough.directive.ts
@@ -0,0 +1,32 @@
+import { Directive,ElementRef,HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appStrikethrough]'
+})
+export class StrikethroughDirective {
+
+    
+  constructor(private elem: ElementRef){}
+
+  @HostListener("click") onClicks(){
+    this.textDeco("line-through")
+  }
+
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("None")
+  }
+
+  private textDeco(action:string){
+    this.elem.nativeElement.style.textDecoration=action;
+
+  }
+}
+
+// import { StrikethroughDirective } from './strikethrough.directive';
+
+// describe('StrikethroughDirective', () => {
+//   it('should create an instance', () => {
+//     const directive = new StrikethroughDirective();
+//     expect(directive).toBeTruthy();
+//   });
+// });
